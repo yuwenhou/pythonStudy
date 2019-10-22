5f4ae42767b6b843ccb37af4b8c3d6e53afb2f1e
Juven <houyuwen2014@163.com>
2019-10-22 10:56:15 +0800

    装饰器

--- 6.mini-web框架/6.3 装饰器/02 对于有参数无返回值进行装饰.py
+++ 6.mini-web框架/6.3 装饰器/02 对于有参数无返回值进行装饰.py
@@ -1,24 +1,22 @@
 def set_func(func):
 
-    def call_func():
-        print("----这是权限验证1-----")
-        func()
-
-    def error():
-        print("密码错误")
+    def call_func(num):
+        password = input("请输入你的密码")
+        if password == "123":
+            print("----这是权限验证1-----")
+            func(num)
+        else:
+            print("密码错误")
     # return call_func
-    password = input("请输入你的密码")
-    if password == "123456":
-        return call_func
-    else:
-        return error
+
+    return call_func
 
 @set_func # 等价于test1 = set_func(test1)
-def test1():
-    print("-------test1-------")
+def test1(num):
+    print("-------test1-------%s"%num)
 
 
-test1()
+test1(100)
 
 
 
