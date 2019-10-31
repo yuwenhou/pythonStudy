c5b040c54af875541214a41398041b9ed9632834
Juven <houyuwen2014@163.com>
2019-10-31 10:42:18 +0800

    日志功能

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-def sum_numbers(num):
-    print('num:%d' % num)
-    if num == 1:
-        return 1
-    temp = sum_numbers(num - 1)
-    print('-----temp:%d' % temp)
-    return num + temp
-
-
-result = sum_numbers(10)
-print(result)
+ban = True
+for i in range(6,23):
+    s = "%d:00 - %d:30"%(i,i)
+    print(s)
+    s = "%d:30 - %d:00"%(i, i+1)
+    print(s)
